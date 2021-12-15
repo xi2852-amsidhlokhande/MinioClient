522d541ad94c9ee4d05aec266acba2dd8b902ae8
Amsidh.Lokhande <amsidh.lokhande@xebia.com>
2021-12-15 14:22:27 +0530

    Buck Changes

--- src/main/java/com/amsidh/mvc/service/FileService.java
+++ src/main/java/com/amsidh/mvc/service/FileService.java
@@ -3,13 +3,13 @@ package com.amsidh.mvc.service;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-
 public interface FileService {
 
-    ResponseEntity uploadFile(String bucketName, MultipartFile file);
+    ResponseEntity uploadFile(String bucketName, MultipartFile file) throws Exception;
 
-    ResponseEntity putFile(String bucketName, MultipartFile multipartFile);
+    ResponseEntity putFile(String bucketName, MultipartFile multipartFile) throws Exception;
 
-    ResponseEntity downloadFile(String bucketName, String fileName);
+    ResponseEntity downloadFile(String bucketName, String fileName) throws Exception;
+
+    ResponseEntity totalFilesInBucket(String bucketName) throws Exception;
 }
--- src/main/java/com/amsidh/mvc/service/impl/FileServiceImpl.java
+++ src/main/java/com/amsidh/mvc/service/impl/FileServiceImpl.java
@@ -1,7 +1,11 @@
 package com.amsidh.mvc.service.impl;
 
+import com.amsidh.mvc.service.FileService;
 import com.amsidh.mvc.service.MinioService;
-import io.minio.*;
+import io.minio.GetObjectResponse;
+import io.minio.ObjectWriteResponse;
+import io.minio.Result;
+import io.minio.messages.Item;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,14 +17,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.amsidh.mvc.service.FileService;
-
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.text.SimpleDateFormat;
 import java.util.UUID;
+import java.util.stream.StreamSupport;
 
 @Service
 public class FileServiceImpl implements FileService {
@@ -32,46 +33,35 @@ public class FileServiceImpl implements FileService {
     private String fileLocation;
 
     @Override
-    public ResponseEntity uploadFile(String bucketName, MultipartFile file) {
-        try {
-            Path filePath = Paths.get(fileLocation + System.currentTimeMillis() + "-" + file.getOriginalFilename());
-            Files.write(filePath, file.getBytes());
-            ObjectWriteResponse response = minioService.uploadObject(bucketName, file.getOriginalFilename(), filePath);
-            return new ResponseEntity("File uploaded Successfully & file name :" + response.object(), HttpStatus.OK);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new ResponseEntity("Failed to upload the file", HttpStatus.BAD_REQUEST);
-        }
+    public ResponseEntity uploadFile(String bucketName, MultipartFile file) throws Exception {
+        Path filePath = Paths.get(fileLocation + System.currentTimeMillis() + "-" + file.getOriginalFilename());
+        Files.write(filePath, file.getBytes());
+        ObjectWriteResponse response = minioService.uploadObject(bucketName, file.getOriginalFilename(), filePath);
+        return new ResponseEntity("File uploaded Successfully & file name :" + response.object(), HttpStatus.OK);
     }
 
     @Override
-    public ResponseEntity putFile(String bucketName, MultipartFile multipartFile) {
-        try {
-            String objectName = System.currentTimeMillis() + "-" + UUID.nameUUIDFromBytes(multipartFile.getBytes()) + multipartFile.getOriginalFilename();
-            ObjectWriteResponse objectWriteResponse = minioService.putObject(bucketName, objectName, multipartFile.getInputStream(), multipartFile.getContentType());
-            return new ResponseEntity("File put successfully & file name " + objectWriteResponse.object(), HttpStatus.OK);
-        } catch (Exception exception) {
-            exception.printStackTrace();
-            return new ResponseEntity("Failed to put the file", HttpStatus.BAD_REQUEST);
-        }
-
+    public ResponseEntity putFile(String bucketName, MultipartFile multipartFile) throws Exception {
+        String objectName = System.currentTimeMillis() + "-" + UUID.nameUUIDFromBytes(multipartFile.getBytes()) + multipartFile.getOriginalFilename();
+        ObjectWriteResponse objectWriteResponse = minioService.putObject(bucketName, objectName, multipartFile.getInputStream(), multipartFile.getContentType());
+        return new ResponseEntity("File put successfully & file name " + objectWriteResponse.object(), HttpStatus.OK);
     }
 
     @Override
-    public ResponseEntity downloadFile(String bucketName, String fileName) {
-
-        try {
-            GetObjectResponse objectResponse = minioService.getObject(bucketName, fileName);
-            byte[] content = IOUtils.toByteArray(objectResponse);
-            objectResponse.close();
-            return ResponseEntity.ok().contentType(MediaType.parseMediaType(MediaType.APPLICATION_PDF_VALUE))
-                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
-                    .body(new ByteArrayResource(content));
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new ResponseEntity("Failed to upload the file", HttpStatus.BAD_REQUEST);
-        }
+    public ResponseEntity downloadFile(String bucketName, String fileName) throws Exception {
+        GetObjectResponse objectResponse = minioService.getObject(bucketName, fileName);
+        byte[] content = IOUtils.toByteArray(objectResponse);
+        objectResponse.close();
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType(MediaType.APPLICATION_PDF_VALUE))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                .body(new ByteArrayResource(content));
     }
 
+    @Override
+    public ResponseEntity totalFilesInBucket(String bucketName) throws Exception {
+        Iterable<Result<Item>> objectResponse = minioService.objectsInBucket(bucketName);
+        long bucketSize = StreamSupport.stream(objectResponse.spliterator(), false).count();
+        return ResponseEntity.ok().body("Total Bucket Size :" + bucketSize);
+    }
 
 }
